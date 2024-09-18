d485bcc73e4b4a2ab419ed056efa5b4d7dd2b596
igornfaustino <igor@nfaustino.com>
2024-09-18 19:33:55 -0300

    aula 17/09

--- loops.js
+++ loops.js
@@ -36,3 +36,75 @@ for (let i = 1; i < 501; i += 2) {
         console.log(i)
     }
 }
+
+for (let i = 0; i < 11; i++) { // i = i + 1
+    console.log(i)
+}
+
+let x = 0;
+while (x < 11) {
+    console.log(x)
+    x++
+}
+
+let num1 = 1
+while (num1 < 1000) {
+    num1 = num1 * 2 // num *= 2
+    console.log(num1)
+}
+
+let num2 = 1
+while (num2 < 1000) {
+    num2 = num2 + num2 // num2 += num2
+    console.log(num2)
+}
+
+
+let y = 10
+do {
+    console.log("entrei")
+} while (y > 100)
+
+
+let resultado
+const valor = 4
+let qtdTentativas = 0
+
+do {
+    resultado = Math.floor(Math.random() * 6) + 1
+    qtdTentativas++
+    console.log("Resultado:", resultado)
+} while (resultado !== valor)
+
+console.log(`Levou ${qtdTentativas} para tirar o numero ${valor}`)
+
+
+qtdTentativas = 1
+resultado = Math.floor(Math.random() * 6) + 1
+while (resultado !== valor) {
+    resultado = Math.floor(Math.random() * 6) + 1
+    qtdTentativas++
+    console.log("Resultado:", resultado)
+}
+console.log(`Levou ${qtdTentativas} para tirar o numero ${valor}`)
+
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) break
+}
+
+let fat = 1
+for (let i = 1; i <= 5; i++) {
+    fat = fat * i
+}
+
+console.log(fat)
+
+const size = 20
+for (let i = 0; i < size; i++) {
+    let linha = '*'
+    for (j = 0; j < i; j++) {
+        linha = linha + ' *'
+    }
+    console.log(linha)
+}
