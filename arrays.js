cb72be1c86d47a7a2736ccc03ae1faa69dc89f37
igornfaustino <igor@nfaustino.com>
2024-10-01 22:04:55 -0300

    aula 01/10

--- arrays.js
+++ arrays.js
@@ -38,3 +38,20 @@ const calcMediaArr = (notas) => {
 }
 
 console.log(calcMediaArr([10, 1, 2]))
+
+
+const nums = [-10, 2, 100, -1, 40, -2, 0]
+
+let qtdNumsNegativos = 0
+let soma = 0
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    if (num < 0) {
+        qtdNumsNegativos++
+    } else {
+        soma += num
+    }
+}
+
+console.log("Soma dos numeros positivos é:", soma)
+console.log("qtd dos numeros negativos é:", qtdNumsNegativos)
